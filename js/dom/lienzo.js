bae4f6aa77a85697d2eec83338a7c585217ede00
Arokaap <aaron.david.nagore@gmail.com>
2023-01-27 14:18:37 +0100

    Metodos para generar y seleccionar pincel

--- js/dom/lienzo.js
+++ js/dom/lienzo.js
@@ -1,7 +1,8 @@
 const d = document;
 
-export function generarTabla() {
-  const $table = d.createElement("table");
+export function generarTabla(div) {
+  const $tabla = d.createElement("table");
+  $tabla.classList.add("tablerodibujo");
 
   for (let i = 0; i < 30; i++) {
     const $tr = d.createElement("tr");
@@ -9,8 +10,61 @@ export function generarTabla() {
       const $td = d.createElement("td");
       $tr.appendChild($td);
     }
-    $table.appendChild($tr);
+    $tabla.appendChild($tr);
   }
 
-  console.log($table);
+  const $div = d.getElementById(div);
+  $div.appendChild($tabla);
+  console.log($div);
+}
+
+export function seleccionarPincel() {
+  d.addEventListener("click", (e) => {
+    const $color1 = d.querySelector(".color1"),
+      $color2 = d.querySelector(".color2"),
+      $color3 = d.querySelector(".color3"),
+      $color4 = d.querySelector(".color4"),
+      $color5 = d.querySelector(".color5"),
+      $color6 = d.querySelector(".color6");
+
+    if (e.target == $color1) {
+      cambiarColorSeleccionado();
+      $color1.classList.add("seleccionado");
+    }
+
+    if (e.target == $color2) {
+      cambiarColorSeleccionado();
+      $color2.classList.add("seleccionado");
+    }
+
+    if (e.target == $color3) {
+      cambiarColorSeleccionado();
+      $color3.classList.add("seleccionado");
+    }
+
+    if (e.target == $color4) {
+      cambiarColorSeleccionado();
+      $color4.classList.add("seleccionado");
+    }
+
+    if (e.target == $color5) {
+      cambiarColorSeleccionado();
+      $color5.classList.add("seleccionado");
+    }
+
+    if (e.target == $color6) {
+      cambiarColorSeleccionado();
+      $color6.classList.add("seleccionado");
+    }
+  });
+}
+
+function cambiarColorSeleccionado() {
+  const $colores = d.querySelectorAll(".seleccionado");
+
+  for (let i = 0; i < 6; i++) {
+    if ($colores[i]) {
+      $colores[i].classList.remove("seleccionado");
+    }
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 //En este fichero cargaré las funcionalidades de la página
 
-import { generarTabla } from "./dom/lienzo.js";
+import { generarTabla, seleccionarPincel } from "./dom/lienzo.js";
 
 const d = document; // Por ahorrarme escribir document cada que quiera usarlo, es una nueva practica que estoy usando para ahorrarme tiempo.
 
 d.addEventListener("DOMContentLoaded", () => {
-  generarTabla();
+  generarTabla("zonadibujo");
+  seleccionarPincel();
 });
